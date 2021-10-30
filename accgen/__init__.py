e87a7b0056bf6c334665d59bdc18be78c1caf877
Amarnath C <amarnathcharichilil@gmail.com>
2021-10-30 19:06:55 +0530

    Update __init__.py

--- accgen/__init__.py
+++ accgen/__init__.py
@@ -6,6 +6,7 @@ API_HASH = getenv("API_HASH")
 TOKEN = getenv("TOKEN")
 FJOIN_CHAT_ID = int(getenv("FJOIN_CHAT_ID"))
 SUDO = list({int(x) for x in getenv("SUDO").split()}) if getenv("SUDO") else []
+DB_URL = getenv("DB_URL")
 
 bot = TelegramClient (None, API_KEY, API_HASH)
 
