c5f8ff1ee00f2b6ac458f577807168960d773829
Amarnath C <amarnathcharichilil@gmail.com>
2021-10-30 19:11:48 +0530

    Update __main__.py

--- accgen/__main__.py
+++ accgen/__main__.py
@@ -1,6 +1,6 @@
 from accgen import bot, TOKEN
 import sys
-from telethon import events
+from telethon import events, Button, functions
 
 try:
   bot.start(bot_token=TOKEN)
@@ -9,6 +9,16 @@ except:
 
 @bot.on(events.NewMessage(pattern="^[?!/]start$", func=lambda e: e.is_private))
 async def start_msg(e):
- await e.reply("**🔥Hey Pro, Welcome To Accounts Generator Bot🔥**\nClick /cmds to Know more")
+ try:
+  p = await bot(functions.channels.GetParticipantRequest(e.chat_id, e.sender_id))
+  p = True
+ except:
+  p = False
+ start_cmd = "**🔥Hey Pro, Welcome To Accounts Generator Bot🔥**\nClick /cmds to Know more"
+ if not p:
+  start_cmd += "\n\nTo use the Bot You Have to join **[Piro Giveaways]**(https://t.me/Piro_giveaways)"
+ await e.respond(start_cmd, buttons=Button.url("Join Channel", "https://t.me/Piro_giveaways") if not p else None)
+
+
 
 @bot.run_until_disconnected()
